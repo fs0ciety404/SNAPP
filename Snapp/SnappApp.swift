20f9b6630d7c0974b8b01067910bbacb12290d8c
Stusio <france.destasio@studenti.unina.it>
2023-03-10 16:04:34 +0100

    Initial Commit

---
+++ Snapp/SnappApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnappApp.swift
+//  Snapp
+//
+//  Created by Francesco De Stasio on 10/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SnappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
