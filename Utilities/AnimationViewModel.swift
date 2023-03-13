27a9a562e12b76abd55d8e2b27e900413a31b7a4
fs0ciety404 <113516391+fs0ciety404@users.noreply.github.com>
2023-03-13 16:47:21 +0100

    Updated Snapp Animation

--- Utilities/AnimationViewModel.swift
+++ Utilities/AnimationViewModel.swift
@@ -21,25 +21,44 @@ struct MyAnimation : View {
     
     @State private var wawe1 = false
     
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View{
         
         ZStack{
             
-            Circle()
-
-                .stroke(lineWidth: 2)
-                .frame(width: 100,height: 100)
-                .foregroundColor(Color.gray.opacity(1))
-                .scaleEffect(wawe ? 2 : 1)
-                .opacity(wawe ? 0 : 1)
-                .shadow(color: .white, radius: 20)
-                .brightness(5)
-                .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true).speed(0.5))
-                .onAppear() {
-                    self.wawe.toggle()
-                }
-
-            Image("Logo")
+            if colorScheme == .light {
+                
+                Circle()
+                    .stroke(lineWidth: 2)
+                    .frame(width: 100,height: 100)
+                    .foregroundColor(Color.gray.opacity(1))
+                    .scaleEffect(wawe ? 2 : 1)
+                    .opacity(wawe ? 0 : 1)
+                    .shadow(color: .white, radius: 20)
+                    .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true).speed(0.5))
+                    .onAppear() {
+                        self.wawe.toggle()
+                    }
+                
+            } else {
+                
+                Circle()
+                    .stroke(lineWidth: 2)
+                    .frame(width: 100,height: 100)
+                    .foregroundColor(Color.gray.opacity(1))
+                    .scaleEffect(wawe ? 2 : 1)
+                    .opacity(wawe ? 0 : 1)
+                    .shadow(color: .white, radius: 20)
+                    .brightness(5)
+                    .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true).speed(0.5))
+                    .onAppear() {
+                        self.wawe.toggle()
+                    }
+                
+            }
+            
+            Image("LOGO")
                 .frame(width: 50, height: 50)
                 .scaleEffect(0.45)
                 .shadow(radius: 25)
