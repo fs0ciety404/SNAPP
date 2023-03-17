1fe92e0e110416b8ca6d5ab2e76c98f069e3d01f
fs0ciety404 <113516391+fs0ciety404@users.noreply.github.com>
2023-03-17 14:46:59 +0100

    Created 2 new views to use for the app splashscreen in both devices. file in SharedView folder. Added 3 gif to the project and new assets

---
+++ SharedView/SplashscreenViewWatchOS.swift
@@ -0,0 +1,23 @@
+//
+//  SplashscreenViewWatchOS.swift
+//  SnappWatch Watch App
+//
+//  Created by Davide Ragosta on 17/03/23.
+//
+
+import SwiftUI
+
+struct SplashscreenViewWatchOS: View {
+    var body: some View {
+        ZStack{
+            VStack{
+            }
+        }
+    }
+}
+
+struct SplashscreenViewWatchOS_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashscreenViewWatchOS()
+    }
+}
